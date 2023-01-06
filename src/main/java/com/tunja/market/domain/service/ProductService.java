5ca5a8ce3b4f9769b04f3a656c97a06051d796e3
sebastian <sebastian.martinez07@uptc.edu.co>
2023-01-06 17:16:00 -0500

    add Service product

---
+++ src/main/java/com/tunja/market/domain/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.tunja.market.domain.service;
+
+import com.tunja.market.domain.Product;
+import com.tunja.market.domain.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private IProductRepository iProductRepository;
+
+    public List<Product> getAll(){
+        return iProductRepository.getAll();
+    }
+
+     public Optional<Product> getProduct(int productId) {
+        return iProductRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return iProductRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return iProductRepository.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return getProduct(productId).map(product -> {
+            iProductRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+
+}
--- src/main/java/com/tunja/market/persistence/ProductRepository.java
+++ src/main/java/com/tunja/market/persistence/ProductRepository.java
@@ -1,40 +1,57 @@
 package com.tunja.market.persistence;
 
+import com.tunja.market.domain.Product;
+import com.tunja.market.domain.repository.IProductRepository;
 import com.tunja.market.persistence.crud.ProductCrudRepository;
 import com.tunja.market.persistence.entity.ProductEntity;
+import com.tunja.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements IProductRepository {
 
+    @Autowired
     private ProductCrudRepository productCrudRepository;
 
-    public List<ProductEntity> getAll(){
-        return (List<ProductEntity>) productCrudRepository.findAll();
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Override
+    public List<Product> getAll() {
+        List<ProductEntity> products = (List<ProductEntity>) productCrudRepository.findAll();
+        return productMapper.toProducts(products);
     }
 
-    public List<ProductEntity> getByCategory(int idCategory){
-        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<ProductEntity> products = productCrudRepository.findByIdCategoryOrderByNameAsc(categoryId);
+        return Optional.of(productMapper.toProducts(products));
     }
 
-    public Optional<List<ProductEntity>> getEscasos(int stock){
-        return productCrudRepository.findByStockLessThanAndState(stock,true);
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<ProductEntity>> products = productCrudRepository.findByStockLessThanAndState(quantity,true);
+        return products.map(prods -> productMapper.toProducts(prods));
     }
 
-    public Optional<ProductEntity> getProductById(int idProduct){
-        return productCrudRepository.findById(idProduct);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        Optional<ProductEntity> productEntity = productCrudRepository.findById(productId);
+        return productEntity.map(prods -> productMapper.toProduct(prods));
     }
 
-    public ProductEntity saveProduct(ProductEntity product){
-        return productCrudRepository.save(product);
+    @Override
+    public Product save(Product product) {
+        ProductEntity productEntity = productMapper.toProductEntity(product);
+        return productMapper.toProduct(productCrudRepository.save(productEntity));
     }
 
-    public void deleteProduct(int idProduct){
-        productCrudRepository.deleteById(idProduct);
+    @Override
+    public void delete(int productId) {
+        productCrudRepository.deleteById(productId);
     }
-
-
 }
